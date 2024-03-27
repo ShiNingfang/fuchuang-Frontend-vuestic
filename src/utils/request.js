eeb432eb4c28cc3d6a01879ec6a6b85df7f70cb9
snf <3305399086@qq.com>
2024-03-27 18:12:47 +0800

    port:3000 2

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,8 @@ const service = axios.create({
   timeout: 5000, // request timeout
 })
 
+console.log(import.meta.env.VITE_APP_BASE_API)
+
 // request interceptor
 service.interceptors.request.use(
   (config) => {
